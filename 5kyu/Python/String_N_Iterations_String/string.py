717fc058484b9586c2d1beae23bd3f3b1d47619c
Bloumbs <baysicsss@gmail.com>
2020-01-29 15:55:47 -0600

    updated string.py with completed code

--- 5kyu/Python/String_N_Iterations_String/string.py
+++ 5kyu/Python/String_N_Iterations_String/string.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
 
-def main(string, n):
-   even = ""
-   odd = ""
+# String -> N iterations -> String
+# https://www.codewars.com/kata/5ae43ed6252e666a6b0000a4/train/python
 
-   for i in range(n):
-      # string = ''.join(list(string)[::2] + list(string)[1::2])
-      for c in range(len(string)):
-         if (c % 2) == 0:
-            even += string[c]
-         else:
-            odd += string[c]
+def jumbled_string_mod(s, n):
+    # "jumble" until it gets back to its orignal string.
+    # last iteration is the number to mod 'n' by
+    og = s
+    print(f'Start: {s}, n: {n}')
+    for i in range(1, n+1):
+        s = ''.join(list(s)[::2] + list(s)[1::2])
+        if s == og:
+            return f'\nBack to OG: {s}, {i} iterations.\n'
+        print(f'{i} Change: {s}')
 
-      string = even + odd
-      even = ""
-      odd = ""
+    return f'Final: {s}, {i} iterations.\n'
 
-   return string
+def jumbled_string(s, n):
+    m = 0
+    og = s
+    # first "jumble" loop to get string back to original.
+    for i in range(1, n+1):
+        s = ''.join(list(s)[::2] + list(s)[1::2])
+        if s == og:
+            m = i
+            break
+
+    # then if 'm'(iteration number) is greater than 0 (it returned to original string)
+    # use 'm' to mod original 'n' (n % m). loop that many times, return jumbled string.
+    if m == 0: return s
+    s = og
+    n = n % m
+    for _ in range(n):
+        s = ''.join(list(s)[::2] + list(s)[1::2])
+
+    return s
 
 if __name__ == "__main__":
-   n = 2
-   string = "better example"
-   print(main(string, n))
+    print("Sc o!uhWw",jumbled_string("Such Wow!",1))
+    print("bexltept merae",jumbled_string("better example",2))
+    print("qtorieuwy",jumbled_string("qwertyuio",2))
+    print("qtorieuwy", jumbled_string("qwertyuio",8764390076))
+    print("Gtsegenri",jumbled_string("Greetings",8))
+    print("criyinodedsgufrnodnoser", jumbled_string("codingisfornerdsyounerd",10101010))
