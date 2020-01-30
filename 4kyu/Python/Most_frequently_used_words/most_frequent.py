2aae603c45bfdf4b67ca3f052f38014f33abfe34
Bloumbs <baysicsss@gmail.com>
2020-01-30 03:45:45 -0600

    added sample tests and kata info

--- 4kyu/Python/Most_frequently_used_words/most_frequent.py
+++ 4kyu/Python/Most_frequently_used_words/most_frequent.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
+# Most frequently used words in a text
+# https://www.codewars.com/kata/51e056fe544cf36c410000fb/train/python
+
 import re
 
 # regex along the way:
 # [,\.!?:;><!@#$%^&*\(\{\[\]\}\)-_=+|/~`]
 # ([,\\.!?:;><!@#$%^&*\(\{\[\]\}\)_=+|/~`]|[-'\ ]+[-'][-'\ ]+)
 
-def main(text):
+def top_3_words(text):
     # print("Text:", repr(text))
     d = {}
     regex = re.compile("([-,\\.!?:;><!@#$%^&*\(\{\[\]\}\)_=+|/~`]|[-'\ ]+[-'][-'\ ]+)")
     text = regex.sub(' ', text.lower())
-    print("After", repr(text), "\n")
+    # print("After", repr(text), "\n")
 
     for w in text.lower().split():
         if not w in d:
@@ -19,14 +22,25 @@ def main(text):
         else:
             d[w] += 1
 
-    print(d)
+    # print(d)
     results = sorted(d, key=d.get, reverse=True)[:3]
 
     if len(results) > 3:
         return results[:3]
     else:
         return results
-  
+
 if __name__ == "__main__":
-    s = """XhIZSbkpmj!'NekkM/-/'NekkM!zHrTlGkfgS.!hBC;;!-qBkglucGNt?-!!.hBC   ;BMnnkvtpN? _,!zHrTlGkfgS?,zcHVbxdnOC!._KpltvH ;,.'sD,'NekkM?-BMnnkvtpN;_--hBC//;.!hBC,TFpDmpMuT;;:,/hBC.cj'g ,_; DGYckbjzhC, .qBkglucGNt?:,mgmMTud!::.'NekkM 'NekkM:._-?mgmMTud,TFpDmpMuT/?mgmMTud/zHrTlGkfgS;qBkglucGNt!zHrTlGkfgS ,_  hBC:;?qBkglucGNt?,cj'g!?;?-'NekkM 'sD?;;_djm,; mgmMTud ?_ -TFpDmpMuT!-/-_'sD!?/,zHrTlGkfgS:_/bVEocQmwe:??_!zcHVbxdnOC   ;:zHrTlGkfgS;-_-?qBkglucGNt/-!!/djm-!?,cj'g,_ !.'sD/djm-'NekkM!--'NekkM:./'NekkM.?.,'sD;!-'NekkM?-,KpltvH:cj'g!_zHrTlGkfgS;?-;?BMnnkvtpN,.,!!djm;,/KpltvH,,!:,'NekkM:;!vlCx-;-;qBkglucGNt;-!zHrTlGkfgS-_:,/qBkglucGNt:?UPyxqyuFcV,,_zHrTlGkfgS,_zcHVbxdnOC;:DGYckbjzhC ?!::BMnnkvtpN__-bVEocQmwe! zHrTlGkfgS.,,bVEocQmwe_zcHVbxdnOC--;,BMnnkvtpN,;;;zHrTlGkfgS/:?_-zHrTlGkfgS!.;cj'g:;aFvXrhssXk_:;'sD,/?UPyxqyuFcV-DGYckbjzhC:hBC/:-;_mgmMTud!;-.qBkglucGNt/vlCx ./djm,;:! hBC !;lZZ?,;-qBkglucGNt;/;/djm?/UPyxqyuFcV .; ?TFpDmpMuT,hBC.?/djm?//:BMnnkvtpN- ;:aFvXrhssXk-!_- DGYckbjzhC:/;__hBC;  !,'sD_/.;'NekkM:'NekkM.hBC- zHrTlGkfgS_;DGYckbjzhC,?/mgmMTud ?zHrTlGkfgS??'NekkM,??qBkglucGNt;:-/ hBC:- 'sD-,._?'NekkM,djm -_-!zcHVbxdnOC?,!KpltvH:!,zcHVbxdnOC:  -.qBkglucGNt!-'NekkM-.-?'NekkM.XhIZSbkpmj:qBkglucGNt?-zHrTlGkfgS zcHVbxdnOC__ :djm _?;-hBC?_mgmMTud 'NekkM?_,,:vlCx .zHrTlGkfgS-::?KpltvH-/!zHrTlGkfgS/hBC/. /qBkglucGNt!.hBC;!;'sD;,..DGYckbjzhC:.:'NekkM;,/ DGYckbjzhC?:.?BMnnkvtpN-?:qBkglucGNt/?;!mgmMTud?,!,'sD.!TFpDmpMuT!:!_lZZ?!BMnnkvtpN- ?mgmMTud:/:TFpDmpMuT?;hBC_. ,;DGYckbjzhC,/; :'sD,!DGYckbjzhC!;?mgmMTud!,:?XhIZSbkpmj;: 'sD;-zcHVbxdnOC-/?'NekkM!-?-_mgmMTud?,?BMnnkvtpN.  'NekkM.!?!.djm:?-; 'sD_;_./'sD_zHrTlGkfgS- cj'g;!:aFvXrhssXk XhIZSbkpmj;/!XhIZSbkpmj_'NekkM_bVEocQmwe;,qBkglucGNt?'NekkM-; /?djm,qBkglucGNt-::mgmMTud,-/hBC?.,'NekkM:.;..djm_!:BMnnkvtpN_.mgmMTud/hBC-!_:zHrTlGkfgS_?.-'sD.., ,TFpDmpMuT_?:'sD;;!zHrTlGkfgS,qBkglucGNt.vlCx:;-/zHrTlGkfgS?;?!,djm..;!/zHrTlGkfgS.//aFvXrhssXk;zHrTlGkfgS.;,-'NekkM-!!/'NekkM:- . bVEocQmwe?.-djm/'NekkM;,;qBkglucGNt!/-qBkglucGNt.;XgpA,!'sD_/qBkglucGNt-_-_ 'sD!UPyxqyuFcV/djm?bVEocQmwe?./mgmMTud,! ,-zcHVbxdnOC! ,-hBC/zHrTlGkfgS/'NekkM!,cj'g._:!BMnnkvtpN! zHrTlGkfgS;?djm!/-qBkglucGNt/-cj'g;. djm?aFvXrhssXk-:;KpltvH_!zHrTlGkfgS-:/!zcHVbxdnOC?! ,qBkglucGNt:?_lZZ/??-;zcHVbxdnOC/vlCx hBC.zHrTlGkfgS/_,zcHVbxdnOC,?mgmMTud.'NekkM_- 'sD_/!cj'g.zHrTlGkfgS;:TFpDmpMuT._;KpltvH/'NekkM;lZZ--?:."""
-    print(main(s))
+    print(top_3_words("a a a  b  c c  d d d d  e e e e e"), ["e", "d", "a"])
+    print(top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"), ["e", "ddd", "aa"])
+    print(top_3_words("  //wont won't won't "), ["won't", "wont"])
+    print(top_3_words("  , e   .. "), ["e"])
+    print(top_3_words("  ...  "), [])
+    print(top_3_words("  '  "), [])
+    print(top_3_words("  '''  "), [])
+    print(top_3_words("""In a village of La Mancha, the name of which I have no desire to call to
+    mind, there lived not long since one of those gentlemen that keep a lance
+    in the lance-rack, an old buckler, a lean hack, and a greyhound for
+    coursing. An olla of rather more beef than mutton, a salad on most
+    nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+    on Sundays, made away with three-quarters of his income."""), ["a", "of", "on"])
