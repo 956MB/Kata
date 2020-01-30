ced7719f4f05fd7034669d848292955822ac0a25
Bloumbs <baysicsss@gmail.com>
2020-01-30 03:47:11 -0600

    added sample tests and kata info
    still didnt fix the barely working while loop... oh well

--- 4kyu/Python/Next_bigger_number_with_the_same_digits/next.py
+++ 4kyu/Python/Next_bigger_number_with_the_same_digits/next.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-# Definitely not the most efficent, but it works.
+
+# Next bigger number with the same digits
+# https://www.codewars.com/kata/55983863da40caa2c900004e/train/python
+
+# Definitely not the most efficient, but it works.
 # I had to basically touch and go with the amount of loops to get the test case numbers to work, not the best way to do that... idk...
 
-def main(n):
+def next_bigger(n):
     start = sorted(str(n))
     test = 1
     # print(start)
@@ -12,9 +16,12 @@ def main(n):
             # print(sorted(str(n)))
             return n
         test += 1
-    
+
     return -1
 
 if __name__ == "__main__":
-    n = 3788742
-    print(main(n))
+    print(next_bigger(12),21)
+    print(next_bigger(513),531)
+    print(next_bigger(2017),2071)
+    print(next_bigger(414),441)
+    print(next_bigger(144),414)
